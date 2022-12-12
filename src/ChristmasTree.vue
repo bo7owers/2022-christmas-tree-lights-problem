ccfb3645dd58fafeb1411b17779f9d9c8677d2ad
bo7owers <7orres.rene@proton.me>
2022-12-11 19:57:58 -0500

    Add star and log

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -15,13 +15,39 @@ withDefaults(
 
     <div class="flex flex-row justify-center">
       <!-- Create the tree sections -->
+      <div
+        class="absolute bg-yellow-500 w-4 h-4 flex justify-center items-center z-50 top-5 star"
+        v-if="size === 1"
+      ></div>
+      <div
+        class="absolute bg-yellow-500 w-4 h-4 flex justify-center items-center z-50 top-5 star-inverted"
+        v-if="size === 1"
+      ></div>
       <div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center">
         <ChristmasLights class="cool-bg" />
       </div>
     </div>
   </div>
+  <div class="log">
+    <div class="bg-orange-900 w-10 h-16" v-if="size === 7"></div>
+  </div>
 </template>
 <style scoped>
+.star {
+  scale: 1.5;
+  top: 12.5em;
+  rotate: 45deg;
+}
+.star-inverted {
+  scale: 1.5;
+  top: 12.5em;
+  rotate: 0;
+}
+.log {
+  position: relative;
+  top: 16em;
+  left: -15.25em;
+}
 .cool-bg {
   animation: bg-change 4s ease-in-out infinite;
 }
